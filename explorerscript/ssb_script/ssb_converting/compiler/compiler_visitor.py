89fbb02407bae7ea7426cd7c30a4ffcc263fb152
Capypara <hello@capypara.de>
2024-07-16 18:31:37 +0200

    Try to move parsing logic into wrapper itself

--- explorerscript/ssb_script/ssb_converting/compiler/compiler_visitor.py
+++ explorerscript/ssb_script/ssb_converting/compiler/compiler_visitor.py
@@ -329,3 +329,6 @@ class SsbScriptCompilerVisitor(SsbScriptBaseVisitor):
                 self.routine_infos.append(None)  # type: ignore
                 self.routine_ops.append([])
                 self.named_coroutines.append([])  # type: ignore
+
+    def defaultResult(self) -> None:
+        return None
